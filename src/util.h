eedd355553bb8574ba00b4ae656f40aea4111525
odabugs <odabugs@users.noreply.github.com>
2016-01-02 01:28:40 -0500

    Define min/max macros in util.h

--- src/util.h
+++ src/util.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <time.h>
 
+#undef max
+#undef min
+// this is silly
+#define max(l, r) (((l) > (r)) ? (l) : (r))
+#define min(l, r) (((l) < (r)) ? (l) : (r))
+
 extern void timestamp();
 
 #endif /* UTIL_H */
