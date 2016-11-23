7b9e876743578d3b7e5d090b5ac01510a3099e84
odabugs <odabugs@users.noreply.github.com>
2016-11-23 00:32:44 -0500

    Add winutil.WindowProc()

--- lua/winutil.lua
+++ lua/winutil.lua
@@ -2,6 +2,13 @@ local winutil = {}
 local ffi = require("ffi")
 local winapi = require("winapi")
 
+ffi.cdef[[
+LRESULT WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT DefWindowProcW(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+VOID PostQuitMessage(int nExitCode);
+]]
+local C = ffi.C
+
 -- using actual LPDWORD, LPPOINT, LPRECT etc. won't work with most
 -- functions in the Win32 API (e.g., GetClientRect)
 winutil.dwordBufType = ffi.typeof("DWORD[1]")
@@ -13,6 +20,9 @@ winutil.boolBufType = ffi.typeof("BOOL[1]")
 -- winapi.wcs module uses WCHAR when you ask it for a char buffer via WCS()
 winutil.charSize = ffi.sizeof("WCHAR")
 
+-- window notifications
+winutil.WM_DESTROY = 0x02
+
 function winutil.stringBufferLength(buffer)
 	return (ffi.sizeof(buffer) / winutil.charSize)
 end
@@ -21,6 +31,15 @@ function winutil.makeStringBuffer(n)
 	local newN = (n > 0 and n or 255)
 	local buffer = winapi.WCS(newN)
 	return buffer
+end
+
+function winutil.WindowProc(hwnd, message, wParam, lParam)
+	if message == winutil.WM_DESTROY then
+		C.PostQuitMessage(0)
+	else
+		return C.DefWindowProcW(hwnd, message, wParam, lParam)
+	end
+	return 0
 end
 
 return winutil
