849718335df8b6e5099433e74bff5a1029383245
odabugs <odabugs@users.noreply.github.com>
2016-12-03 23:25:30 -0500

    Add PCSX2_Common.RAMlimit

--- lua/game/pcsx2/common.lua
+++ lua/game/pcsx2/common.lua
@@ -2,6 +2,7 @@ local Game_Common = require("game.common")
 local PCSX2_Common = Game_Common:new()
 
 -- fixed starting address where PCSX2 stores the game's emulated RAM state
-PCSX2_Common.RAMbase = 0x20000000 -- (address range ends at 0x22000000)
+PCSX2_Common.RAMbase = 0x20000000
+PCSX2_Common.RAMlimit = 0x22000000
 
 return PCSX2_Common
