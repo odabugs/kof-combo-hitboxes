831417fad5e5dc453638cc24dbcae008d53e84d0
odabugs <odabugs@users.noreply.github.com>
2016-12-03 23:22:02 -0500

    Add ffiutil.ntypes()

--- lua/ffiutil.lua
+++ lua/ffiutil.lua
@@ -6,4 +6,13 @@ function ffiutil.intptr(ptr)
 	return tonumber(ffi.cast("intptr_t", ptr))
 end
 
+function ffiutil.ntypes(t, n, start)
+	start = (start or 1)
+	local result = {}
+	for i = start, (start + n) - 1 do
+		result[i] = ffi.new(t)
+	end
+	return result
+end
+
 return ffiutil
