c976d04d903840e3b797f761b92a0a53a8b95aab
odabugs <odabugs@users.noreply.github.com>
2016-12-26 13:59:56 -0500

    Fill in some missing box ID -> box type mappings for KOF XI

--- lua/game/pcsx2/kof_xi/boxtypes.lua
+++ lua/game/pcsx2/kof_xi/boxtypes.lua
@@ -20,9 +20,9 @@ local b_t  = "throw"
 
 --  +00h  +01h  +02h  +03h   +04h  +05h  +06h  +07h
 add(b_xx, b_v , b_v , b_vc,  b_vc, b_v , b_vc, b_v ) -- 00h-07h
-add(b_xx, b_xx, b_xx, b_v ,  b_xx, b_xx, b_xx, b_xx) -- 08h-0Fh
-add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 10h-17h
-add(b_xx, b_xx, b_xx, b_xx,  b_g , b_g , b_g , b_v ) -- 18h-1Fh
+add(b_xx, b_xx, b_v , b_v ,  b_v , b_v , b_xx, b_pv) -- 08h-0Fh
+add(b_pv, b_pv, b_pv, b_pv,  b_pv, b_pv, b_pv, b_pv) -- 10h-17h
+add(b_xx, b_pv, b_pv, b_pv,  b_g , b_g , b_g , b_v ) -- 18h-1Fh
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 20h-27h
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 28h-2Fh
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 30h-37h
@@ -33,7 +33,7 @@ add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 48h-4Fh
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 50h-57h
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 58h-5Fh
 add(b_a , b_v , b_v , b_v ,  b_v , b_v , b_v , b_xx) -- 60h-67h
-add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 68h-6Fh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_a , b_a ) -- 68h-6Fh
 add(b_co, b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 70h-77h
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 78h-7Fh
 --  +00h  +01h  +02h  +03h   +04h  +05h  +06h  +07h
@@ -66,8 +66,8 @@ local colormap = {
 	[b_g]  = colors.CYAN,
 	[b_a]  = colors.RED,
 	[b_cl] = colors.BLACK,
-	[b_pv] = colors.YELLOW,
-	[b_pa] = colors.RED,
+	[b_pv] = colors.GREEN,
+	[b_pa] = colors.YELLOW,
 	[b_tv] = colors.WHITE,
 	[b_t]  = colors.MAGENTA,
 }
@@ -83,6 +83,12 @@ end
 
 function boxtypes.colorForType(boxtype)
 	return colormap[boxtype]
+end
+
+function boxtypes.asProjectile(boxtype)
+	if boxtype == b_a then return b_pa
+	elseif boxtype == b_v then return b_pv
+	else return boxtype end
 end
 
 --[[
