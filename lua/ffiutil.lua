26be9a847c9b7592161a1a7298fa5639496435c1
odabugs <odabugs@users.noreply.github.com>
2017-01-22 18:32:56 -0500

    ffiutil.ntypes() now takes a type generator function or a string as its first parameter

--- lua/ffiutil.lua
+++ lua/ffiutil.lua
@@ -6,11 +6,20 @@ function ffiutil.intptr(ptr)
 	return tonumber(ffi.cast("intptr_t", ptr))
 end
 
-function ffiutil.ntypes(t, n, start)
+-- t may be a string (passed to ffi.new()), or a function that returns an
+-- object when called
+function ffiutil.ntypes(t, n, start, ...)
+	if type(t) == "string" then
+		local typename = t
+		t = function() return ffi.new(typename) end
+	elseif type(t) ~= "function" then
+		error("Parameter \"t\" must be a string or a function.")
+	end
+
 	start = (start or 1)
 	local result = {}
 	for i = start, (start + n) - 1 do
-		result[i] = ffi.new(t)
+		result[i] = t(i, ...)
 	end
 	return result
 end
