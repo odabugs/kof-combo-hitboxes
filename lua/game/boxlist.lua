7abe2f536f185febe6bc266e23354e05c7f0f0a2
odabugs <odabugs@users.noreply.github.com>
2017-01-29 17:31:21 -0500

    Add BoxList and BoxSet for drawing hitboxes properly layered by box type

---
+++ lua/game/boxlist.lua
@@ -0,0 +1,34 @@
+local ffiutil = require("ffiutil")
+local BoxList = {}
+
+function BoxList:new(slot, count, slotConstructor, ...)
+	local result = ffiutil.ntypes(slotConstructor, count, 1, slot, ...)
+	result.slot = slot
+	result.count = count
+	result.valid = 0 -- updated each time we add/remove an entry or reset
+
+	setmetatable(result, self)
+	self.__index = self
+	return result
+end
+
+function BoxList:reset()
+	self.valid = 0
+end
+
+function BoxList:add(addFn, ...)
+	assert(self.valid < self.count, "Can't add more entries to this list!")
+	local i = self.valid + 1
+	addFn(self[i], ...)
+	self.valid = i
+end
+
+function BoxList:render(renderFn, ...)
+	local boxesDrawn = 0
+	for i = 1, self.valid do
+		boxesDrawn = boxesDrawn + renderFn(self[i], ...)
+	end
+	return boxesDrawn
+end
+
+return BoxList
---
+++ lua/game/boxset.lua
@@ -0,0 +1,47 @@
+local BoxList = require("game.boxlist")
+local BoxSet = {}
+
+function BoxSet:new(order, countPerLayer, slotConstructor, ...)
+	local numLayers = #order
+	local result = {
+		order = order,
+		numLayers = numLayers,
+		countPerLayer = countPerLayer,
+		totalCount = numLayers * countPerLayer,
+	}
+	local slotToAdd, key
+	for i = 1, numLayers do
+		key = order[i]
+		slotToAdd = BoxList:new(key, countPerLayer, slotConstructor, ...)
+		-- each box list is stored at two table locations;
+		-- the hash slot is used by BoxSet:add(),
+		-- while the array entry is used when iterating over all slots
+		result[key] = slotToAdd
+		result[i] = slotToAdd
+	end
+
+	setmetatable(result, self)
+	self.__index = self
+	for k,v in pairs(result) do print(k,v) end
+	return result
+end
+
+function BoxSet:reset()
+	for i = 1, self.numLayers do
+		self[i]:reset()
+	end
+end
+
+function BoxSet:add(slot, addFn, ...)
+	self[slot]:add(addFn, ...)
+end
+
+function BoxSet:render(renderFn, ...)
+	local boxesDrawn = 0
+	for i = 1, self.numLayers do
+		boxesDrawn = boxesDrawn + self[i]:render(renderFn, ...)
+	end
+	return boxesDrawn
+end
+
+return BoxSet
