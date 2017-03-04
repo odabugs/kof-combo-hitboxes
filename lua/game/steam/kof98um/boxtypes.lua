04906ae621cc13472c22d42034a72e5f32a00c7c
odabugs <odabugs@users.noreply.github.com>
2017-03-04 16:36:09 -0500

    Fix possible crash due to null pointers in KOF98/KOF02

--- lua/game/steam/kof98um/boxtypes.lua
+++ lua/game/steam/kof98um/boxtypes.lua
@@ -26,33 +26,33 @@ add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 18h-1Fh
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 20h-27h
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 28h-2Fh
 add(b_a , b_a , b_a , b_a ,  b_a , b_a , b_a , b_a ) -- 30h-37h
-add(b_g , b_g , b_pv, b_pv,  b_pv, b_pv, b_pv, b_x ) -- 38h-3Fh
+add(b_g , b_g , b_pv, b_pv,  b_pv, b_pv, b_pv, b_xx) -- 38h-3Fh
 --  +00h  +01h  +02h  +03h   +04h  +05h  +06h  +07h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 40h-47h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 48h-4Fh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 50h-57h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 58h-5Fh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 60h-67h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 68h-6Fh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 70h-77h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 78h-7Fh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 40h-47h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 48h-4Fh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 50h-57h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 58h-5Fh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 60h-67h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 68h-6Fh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 70h-77h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 78h-7Fh
 --  +00h  +01h  +02h  +03h   +04h  +05h  +06h  +07h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 80h-87h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 88h-8Fh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 90h-97h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- 98h-9Fh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- A0h-A7h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- A8h-AFh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- B0h-B7h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- B8h-BFh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 80h-87h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 88h-8Fh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 90h-97h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- 98h-9Fh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- A0h-A7h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- A8h-AFh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- B0h-B7h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- B8h-BFh
 --  +00h  +01h  +02h  +03h   +04h  +05h  +06h  +07h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- C0h-C7h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- C8h-CFh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- D0h-D7h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- D8h-DFh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- E0h-E7h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- E8h-EFh
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- F0h-F7h
-add(b_x , b_x , b_x , b_x ,  b_x , b_x , b_x , b_x ) -- F8h-FFh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- C0h-C7h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- C8h-CFh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- D0h-D7h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- D8h-DFh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- E0h-E7h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- E8h-EFh
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- F0h-F7h
+add(b_xx, b_xx, b_xx, b_xx,  b_xx, b_xx, b_xx, b_xx) -- F8h-FFh
 
 return boxtypes
