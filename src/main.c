93e78410bf073456f298916ce42a5de9fc15f3d6
odabugs <odabugs@users.noreply.github.com>
2017-07-11 01:13:29 -0400

    Don't close console window immediately when a Lua script aborts with an error

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <winuser.h>
+#include <conio.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <luajit.h>
@@ -15,6 +16,9 @@ static lRegSet luaExports[] = {
 	{ "directx", lib_directX },
 	{ NULL, NULL } // sentinel
 };
+static char *LUA_LOAD_ERROR_LINE = "Failed to load Lua script: %s\n";
+static char *LUA_RUN_ERROR_LINE = "Error occurred inside Lua script: %s\n";
+void showLuaError(lua_State *L, char *errline);
 
 int WINAPI WinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpArgv, int nShowCmd)
@@ -25,14 +29,12 @@ int WINAPI WinMain(
 	lua_pushcfunction(L, lTraceback);
 	int status = luaL_loadfile(L, "lua/main.lua");
 	if (status != 0) {
-		printf("Failed to load Lua script: %s\n", lua_tostring(L, -1));
-		exit(1);
+		showLuaError(L, LUA_LOAD_ERROR_LINE);
 	}
 	// parse Lua scripts
 	result = lua_pcall(L, 0, 0, 1);
 	if (result != 0) {
-		printf("Error occurred inside Lua script: %s\n", lua_tostring(L, -1));
-		exit(1);
+		showLuaError(L, LUA_RUN_ERROR_LINE);
 	}
 	// call main() in Lua, with hInstance as first argument
 	lua_getglobal(L, "main");
@@ -40,9 +42,16 @@ int WINAPI WinMain(
 	lRegisterAll(L, luaExports, false);
 	result = lua_pcall(L, 2, LUA_MULTRET, 1);
 	if (result != 0) {
-		printf("Error occurred inside Lua script: %s\n", lua_tostring(L, -1));
-		exit(1);
+		showLuaError(L, LUA_RUN_ERROR_LINE);
 	}
 	lua_close(L);
 	return 0;
+}
+
+void showLuaError(lua_State *L, char *errline)
+{
+	printf(errline, lua_tostring(L, -1));
+	printf("Press any key to exit.\n");
+	while (_kbhit() == 0) { Sleep(5); } // wait for keypress
+	exit(1);
 }
